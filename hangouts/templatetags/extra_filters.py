90cdeeec2ae77b2d91ff1d3cef6b0d8cf9b9e7b1
Bingo8 <aini19931225@hotmail.com>
2018-12-20 14:34:44 +0100

    add an example for d3 and backend

--- hangouts/templatetags/extra_filters.py
+++ hangouts/templatetags/extra_filters.py
@@ -1,3 +1,18 @@
 from django import template
 
 register = template.Library()
+
+@register.filter()
+def keys(dict):
+	return dict.keys()
+
+
+@register.filter
+def max_list(l):
+	return max(l)
+
+@register.filter
+def min_list(l):
+	return min(l)
+
+
